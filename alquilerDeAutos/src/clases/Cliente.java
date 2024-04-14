a6000602b65b261f63ca39c318225afea48988dc
Frtocco <164193133+Frtocco@users.noreply.github.com>
2024-04-14 19:14:27 -0300

    Delete alquilerDeAutos directory

--- alquilerDeAutos/src/clases/Cliente.java
+++
@@ -1,9 +0,0 @@
-package clases;
-
-public class Cliente {
-	private String nombreCliente;
-	private String dni;
-	private String direccion;
-	private int numeroCliente;
-	private static int contador;
-}
